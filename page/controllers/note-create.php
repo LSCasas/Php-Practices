533d1df6bc00d136e21e39e593773ecde52fbf7c
Luis Casas <luisscasas45@gmail.com>
2025-01-03 17:58:27 -0600

    FEAT/ Escape Untrusted Input

--- page/controllers/note-create.php
+++ page/controllers/note-create.php
@@ -1,9 +1,15 @@
 <?php
 
+$db = new Database();
+
 $heading = "Create note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $db->query('INSERT INTO notes(body, user_id) 
+    VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
 }
 
 require 'views/note-create.view.php';
--- page/views/note.view.php
+++ page/views/note.view.php
@@ -5,7 +5,7 @@
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <p class="mb-6"><a href="/notes" class="text-blue-500 underline">go back...</a></p>
-        <p><?= $note['body'] ?></P>
+        <p><?= htmlspecialchars($note['body']) ?></P>
 
 
     </div>
